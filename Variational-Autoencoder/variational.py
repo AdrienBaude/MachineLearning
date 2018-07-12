f487735697b877be1fa5d68c621fe99ca4636982
Adrien BAUDE <Vendicare@localhost.localdomain>
2018-07-13 00:10:51 +0200

    commit

--- Variational-Autoencoder/variational.py
+++ Variational-Autoencoder/variational.py
@@ -1,38 +1,27 @@
 # Reference https://wiseodd.github.io/techblog/2016/12/10/variational-autoencoder/
 
 from keras.layers import Input, Dense, Lambda
-from keras.models import Model
+from keras.models import Model, Sequential
 import keras.backend as K
 from keras.datasets import mnist
 from matplotlib import pyplot as plt
 import os
 import numpy as np
-from keras.utils import to_categorical
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, _), (_, _) = mnist.load_data()
 x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
 x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
 x_train /= 255
-x_test /= 255
-
-y_test = to_categorical(y_test, 10)
-
-m = 50
-n_z = 2
-n_epoch = 10
-
-inputs = Input(shape=(784,))
-h_q = Dense(512, activation='relu')(inputs)
-mu = Dense(n_z, activation='linear')(h_q)
-log_sigma = Dense(n_z, activation='linear')(h_q)
 
 def sample_z(args):
     mu, log_sigma = args
-    eps = K.random_normal(shape=(m, n_z), mean=0.)
+    eps = K.random_normal(shape=(50, 2), mean=0.)
     return mu + K.exp(log_sigma / 2) * eps
 
+inputs = Input(shape=(784,))
+h_q = Dense(512, activation='relu')(inputs)
+mu = Dense(2, activation='linear')(h_q)
+log_sigma = Dense(2, activation='linear')(h_q)
 z = Lambda(sample_z)([mu, log_sigma])
 
 decoder_hidden = Dense(512, activation='relu')
@@ -45,7 +34,7 @@ vae = Model(inputs, outputs)
 
 encoder = Model(inputs, mu)
 
-d_in = Input(shape=(n_z,))
+d_in = Input(shape=(2,))
 d_h = decoder_hidden(d_in)
 d_out = decoder_out(d_h)
 decoder = Model(d_in, d_out)
@@ -53,15 +42,15 @@ decoder = Model(d_in, d_out)
 def vae_loss(y_true, y_pred):
     recon = K.sum(K.binary_crossentropy(y_pred, y_true), axis=1)
     kl = 0.5 * K.sum(K.exp(log_sigma) + K.square(mu) - 1. - log_sigma, axis=1)
+
     return recon + kl
 
 vae.compile(optimizer='adam', loss=vae_loss)
 
-
 if os.path.isfile('generator.h5'):
     decoder.load_weights("generator.h5")
 else:
-    vae.fit(x_train, x_train, batch_size=m, nb_epoch=n_epoch)
+    vae.fit(x_train, x_train, batch_size=50, nb_epoch=10)
     decoder.save_weights("generator.h5")
 
 noise = np.random.normal(0, 1, (25, 2))
