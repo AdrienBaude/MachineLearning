95175b90e60944fc6a003c403bf5f1e0d568b2de
AdrienBaude <adrien.baude@hotmail.fr>
2019-07-10 20:20:58 +0200

    commit

---
+++ Classification - Audio - FCN/preprocess.py
@@ -0,0 +1,40 @@
+import glob
+import os
+import librosa
+import numpy as np
+
+
+def extract_feature(file_name):
+    X, sample_rate = librosa.load(file_name)
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T, axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T, axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
+                                              sr=sample_rate).T, axis=0)
+    return mfccs, chroma, mel, contrast, tonnetz
+
+
+def parse_audio_files(parent_dir, sub_dirs, file_ext="*.wav"):
+    features, labels = np.empty((0, 193)), np.empty(0)
+    for label, sub_dir in enumerate(sub_dirs):
+        for fn in glob.glob(os.path.join(parent_dir, sub_dir, file_ext)):
+            try:
+                mfccs, chroma, mel, contrast, tonnetz = extract_feature(fn)
+            except Exception as e:
+                print("Error encountered while parsing file: ", fn)
+                continue
+            ext_features = np.hstack([mfccs, chroma, mel, contrast, tonnetz])
+            features = np.vstack([features, ext_features])
+            labels = np.append(labels, fn.split('\\')[2].split('-')[1])
+    return np.array(features), np.array(labels, dtype=np.int)
+
+
+parent_dir = 'data'
+sub_dirs = ["fold1", "fold2", "fold3", "fold4", "fold5", "fold6", "fold7", "fold8", "fold9", "fold10"]
+
+features, labels = parse_audio_files(parent_dir, sub_dirs)
+
+np.save("X", features)
+np.save("Y", labels)
